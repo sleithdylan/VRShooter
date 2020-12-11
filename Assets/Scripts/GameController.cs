cdc9c8f8a4bde16767784f33e215b9a1a77c57a8
Dylan Sleith <sleithdylan@gmail.com>
2020-12-11 12:56:21 +0000

    Add health & gameover

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
@@ -18,6 +20,14 @@ public class GameController : MonoBehaviour
     public float enemyIntervalDecrement = 0.1f;
     // Enemy Timer
     private float enemyTimer = 0f;
+    // Health Text
+    public TextMeshProUGUI Health;
+    // Health Text
+    public TextMeshProUGUI Gameover;
+    // Enemy Timer
+    private float gameTimer = 0f;
+    // Restart Timer
+    private float restartTimer = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,9 +35,34 @@ public class GameController : MonoBehaviour
         
     }
 
+    void SetHealthText()
+    {
+        Health.text = " <sprite=0> " + player.health;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (player.health > 0)
+        {
+            gameTimer += Time.deltaTime;
+            // Players Health
+            SetHealthText();
+        } else
+        {
+            // End screen
+            Gameover.color = Color.red;
+            Gameover.text = "Game Over!\n";
+            Gameover.text += "\nYou surived for " + Mathf.Floor(gameTimer) + " seconds!";
+
+            restartTimer -= Time.deltaTime;
+            if (restartTimer <= 0f)
+            {
+                // Restart game
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+   
         enemyTimer -= Time.deltaTime;
 
         if (enemyTimer <= 0) 
